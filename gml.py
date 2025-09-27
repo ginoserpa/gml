cbfacb1162b4308190c44c9f4b6363fd1a7b69aa
gino serpa <gino.serpa@gmail.com>
2025-09-27 13:35:18 -0500

    remaking lost work, infrastructure for indesting data done, need to do it globally

--- gml.py
+++ gml.py
@@ -2,20 +2,52 @@ from pathlib import Path
 import pandas as pd
 import yaml
 
+def get_yaml_files():
+    """ 
+    Get a list of the yaml files from the gddb data directory.
+    """
+
+    current_directory = Path().cwd()/'..'/'gdshowsdb'/'data'/'gdshowsdb'
+    yaml_files = [item for item in current_directory.glob('*.yaml')]
+
+    return yaml_files
 
 
+def create_yaml_file_df(yaml_file):
+    dict_list = []
 
-current_directory = Path().cwd()/'..'/'gdshowsdb'/'data'/'gdshowsdb'
-    
-yaml_files = [item for item in current_directory.glob('*.yaml')]
+    # Extract the year from the filename
+    year = yaml_file.name.split('.')[0]
 
+    with open(yaml_file, "r", encoding="utf-8") as f: 
+        data = yaml.safe_load(f)
 
-with open(yaml_files[0], "r", encoding="utf-8") as f: 
-    data = yaml.safe_load(f)
+    for date in data.keys():
+        uuid = data[date][':uuid']
+        venue = data[date][':venue']
+        city = data[date][':city']
+        state = data[date][':state']
+        country = data[date][':country']
 
-print('*** yaml_file: ',yaml_files[0])
+        for set in data[date][':sets']:
+            set_uuid = set[':uuid'] 
+            for item in set[':songs']:
+                song_uuid = item[':uuid']
+                name = item[':name']
+                segued = item[':segued']
+                dict_list.append({'date': date, 
+                                'year': year,
+                                'uuid': uuid,
+                                'venue': venue,
+                                'city': city,
+                                'state': state,
+                                'country': country,
+                                'set_uuid': set_uuid,
+                                'song_uuid': song_uuid,
+                                'name': name,
+                                'segued': segued
+                                })
 
+    df = pd.DataFrame(dict_list)
 
-df = pd.json_normalize(data)
-
-print(df.head(2))
+    return df, data
